458f861da00c1ebc8b361e738b8b727a060a6a9e
Bryan Way <bryanwayb@gmail.com>
2017-01-31 01:19:06 -0600

    Basic template engine works with build process

--- jshtml.js
+++ jshtml.js
@@ -1,36 +1,25 @@
 (function() {
-    this.jshtml.template = function() {
-       var genScript = '(function(';
-       var args = [ ];
-       if(context) {
-           for(var property in context) {
-               if(context.hasOwnProperty(property)) {
-                   genScript += (first ? '' : ',') + property;
-                   args.push(context[property]);
-                   first = false;
-               }
-           }
-       }
-       genScript += '){' + script + '});';
-       exec = vm.runInThisContext(genScript);
-       context.__init_script(this._options.filename || __filename);
+    var compiler = this.jshtml.compiler(true);
+    this.jshtml.template = function(script, context) {
+        script = compiler(script);
 
-       this._function = func = function(thisObject, callback) {
-           if(typeof thisObject === 'function') {
-               callback = thisObject;
-               thisObject = null;
-           }
-
-           if(callback) {
-               context.__complete = function() {
-                   callback(context.__render_end());
-               };
-               exec.apply(thisObject, args);
-           }
-           else {
-               exec.apply(thisObject, args);
-               return context.__render_end();
-           }
-       };
+        var genScript = '(function(';
+        var args = [ ];
+        if(context) {
+            var first = true;
+            for(var property in context) {
+                if(context.hasOwnProperty(property)) {
+                    genScript += (first ? '' : ',') + property;
+                    args.push(context[property]);
+                    first = false;
+                }
+            }
+        }
+        genScript += '){' + script + '});';
+        exec = eval(genScript);
+ 
+        return function(thisObject) {
+            return exec.apply(thisObject, args);
+        };
     };
 }).call(this);
